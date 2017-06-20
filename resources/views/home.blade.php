92abcbd978e8105c008e6879542cd63d288d944d
Hansen Makangiras <hansenmakangiras@gmail.com>
2017-06-20 16:20:56 +0800

    Add laravel passport for api authorization

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+    <example></example>
+    <passport-clients></passport-clients>
+    <passport-authorized-clients></passport-authorized-clients>
+    <passport-personal-access-tokens></passport-personal-access-tokens>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
